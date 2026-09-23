45ac661fcd43db156b86f8177f63d40e6c1fce52
TrueDotBin <330445590+TrueDotBin@users.noreply.github.com>
2026-09-23 19:43:08 +0200

    Kill old tweens

--- scripts/autoload/tween_manager.gd
+++ scripts/autoload/tween_manager.gd
@@ -1,8 +1,17 @@
 extends Node
 
-func create(node: Node, ease_type: Tween.EaseType, trans_type: Tween.TransitionType, parallel: bool = false) -> Tween:	
-	var tween = node.create_tween()
-	tween.set_ease(ease_type)
-	tween.set_trans(trans_type)
-	tween.set_parallel(parallel)
-	return tween
+var _tweens: Dictionary[Node, Tween] = {}
+
+func create(node: Node, ease_type: Tween.EaseType, trans_type: Tween.TransitionType, parallel: bool = false) -> Tween:
+    if node in _tweens:
+        _tweens[node].kill()
+    
+    var tween = node.create_tween()
+
+    tween.set_ease(ease_type)
+    tween.set_trans(trans_type)
+    tween.set_parallel(parallel)
+
+    _tweens[node] = tween
+
+    return tween
