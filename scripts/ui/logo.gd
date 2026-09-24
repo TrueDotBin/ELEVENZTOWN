ae1924c76eda42372df8b79c413d5422d61b2e55
TrueDotBin <330445590+TrueDotBin@users.noreply.github.com>
2026-09-24 16:49:18 +0200

    Make the logo rotate

---
+++ scripts/ui/logo.gd
@@ -0,0 +1,13 @@
+@tool
+
+extends TextureRect
+
+@export var rotation_speed: float = 3
+@export var rotation_amplitude: float = 1.5
+
+var _time: float
+
+func _process(delta: float) -> void:
+	_time += delta
+
+	rotation_degrees = sin(_time * rotation_speed) * rotation_amplitude
