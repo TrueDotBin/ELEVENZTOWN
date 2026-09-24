f67974862bc434682b78b38192299146d8d65454
TrueDotBin <330445590+TrueDotBin@users.noreply.github.com>
2026-09-24 18:14:25 +0200

    Create credits screen

---
+++ scripts/ui/credits_panel.gd
@@ -0,0 +1,8 @@
+extends ColorRect
+class_name CreditsPanel
+
+func _ready() -> void:
+	hide()
+
+func _on_close_credits_button_pressed() -> void:
+	hide()
--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var main_menu_song: AudioStream = AudioLibrary.MENU_SONG
 
+@onready var credits_panel: CreditsPanel = $CreditsPanel
+
 @onready var play_button: Button = $ButtonContainer/PlayButton
 @onready var options_button: Button = $ButtonContainer/OptionsButton
 @onready var credits_button: Button = $ButtonContainer/CreditsButton
@@ -9,6 +11,7 @@ extends Control
 
 func _connect_button_signals() -> void:
 	leave_button.pressed.connect(GameManager.quit_game)
+	credits_button.pressed.connect(credits_panel.show)
 
 func _ready() -> void:
 	AudioManager.play_music(main_menu_song)
