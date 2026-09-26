4549d01f0df1bcc85bdd7c11459a73519b1eaaa3
TrueDotBin <330445590+TrueDotBin@users.noreply.github.com>
2026-09-26 12:46:28 +0200

    Create basic game scene

---
+++ scripts/game/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+@export_category("Movement")
+@export var speed = 12.0
+@export var jump_velocity = 9.5
+
+@export_category("First-Person Camera")
+@export var camera: Camera3D
+@export var sensitivity: float = 0.005
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouse:
+		rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_velocity
+
+	var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	move_and_slide()
